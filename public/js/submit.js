20f499965e65337c2f300abb23e62b325b4cc059
sultanassi95 <mrsultanassi@gmail.com>
2018-03-20 22:44:41 +0200

    Day1: un-finished demo

---
+++ public/js/submit.js
@@ -0,0 +1,48 @@
+function postData(inputs){
+  fetch('/surve-contract', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(inputs)
+  });
+}
+
+function search(){
+  const orgNum = document.querySelector('#org_nr').value
+  fetch('/search-org', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({orgNum: orgNum})
+  })
+  .then((response) => response.json())
+  .then(function(data){
+    console.log(data);
+  })
+  .catch(function(error){
+    console.log(error);
+  })
+}
+
+function submit(){
+  const inputs = document.querySelectorAll('input');
+  let inputsValues = {}
+  inputs.forEach((input) => {
+    const inputName = input.attributes.name.nodeValue;
+    const inputValue = input.value
+    return inputsValues[inputName] = inputValue
+  });
+  postData(inputsValues);
+}
+
+
+document.querySelector('#submit').addEventListener("click", function(){
+  submit();
+})
+document.querySelector('#search').addEventListener("click", function(){
+  search();
+})
---
+++ src/a.js
@@ -0,0 +1,11 @@
+    name: 'TemplateError',
+    message: 'Unclosed tag',
+    stack: 'Error: Unclosed tag\n    at XTTemplateError (/home/sultan/Desktop/doccer/node_modules/docxtemplater/js/errors.js:19:15)\n    at getUnclosedTagException (/home/sultan/Desktop/doccer/node_modules/docxtemplater/js/errors.js:69:12)\n    at /home/sultan/Desktop/doccer/node_modules/docxtemplater/js/lexer.js:119:17\n    at Array.forEach (native)\n    at getDelimiterErrors (/home/sultan/Desktop/doccer/node_modules/docxtemplater/js/lexer.js:109:19)\n    at Reader.parseDelimiters (/home/sultan/Desktop/doccer/node_modules/docxtemplater/js/lexer.js:239:16)\n    at Object.parse (/home/sultan/Desktop/doccer/node_modules/docxtemplater/js/lexer.js:316:10)\n    at XmlTemplater.parse (/home/sultan/Desktop/doccer/node_modules/docxtemplater/js/xml-templater.js:117:29)\n    at Docxtemplater.compileFile (/home/sultan/Desktop/doccer/node_modules/docxtemplater/js/docxtemplater.js:94:16)\n    at /home/sultan/Desktop/doccer/node_modules/docxtemplater/js/docxtemplater.js:136:13',
+    properties:
+     { xtag: 'subscription_price_list8|',
+       id: 'unclosed_tag',
+       context: '{subscription_price_list8|',
+       offset: 7913,
+       lIndex: undefined,
+       explanation: 'The tag beginning with "{subscript" is unclosed',
+       file: 'word/document.xml' } } ]
